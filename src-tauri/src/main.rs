e96b6b3492ed63a716d27117e7546496fa8a31f7
danithu17 <dnprog1@gmail.com>
2026-02-13 13:18:02 +0530

    Fix build and add strict mode

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,37 +2,53 @@
 
 use std::process::Command;
 use std::sync::{Arc, Mutex};
-use tauri::State;
+use tauri::{State, Manager, Window};
 
 struct AppState {
     is_blocking: Arc<Mutex<bool>>,
+    password: String,
 }
 
 #[tauri::command]
-fn toggle_focus(state: State<'_, AppState>, apps: Vec<String>, status: bool) {
+fn start_focus(state: State<'_, AppState>, apps: Vec<String>, window: Window) {
     let mut is_blocking = state.is_blocking.lock().unwrap();
-    *is_blocking = status;
+    *is_blocking = true;
 
-    if *is_blocking {
-        let is_blocking_clone = Arc::clone(&state.is_blocking);
-        std::thread::spawn(move || {
-            while *is_blocking_clone.lock().unwrap() {
-                for app in &apps {
-                    // Windows command to kill the app process
-                    let _ = Command::new("taskkill")
-                        .args(&["/F", "/IM", app])
-                        .spawn();
-                }
-                std::thread::sleep(std::time::Duration::from_secs(3));
+    // Window eka Fullscreen karala Always on Top dapan
+    window.set_fullscreen(true).unwrap();
+    window.set_always_on_top(true).unwrap();
+
+    let is_blocking_clone = Arc::clone(&state.is_blocking);
+    std::thread::spawn(move || {
+        while *is_blocking_clone.lock().unwrap() {
+            for app in &apps {
+                let _ = Command::new("taskkill").args(&["/F", "/IM", app]).spawn();
             }
-        });
+            std::thread::sleep(std::time::Duration::from_secs(2));
+        }
+    });
+}
+
+#[tauri::command]
+fn stop_focus(state: State<'_, AppState>, input_pass: String, window: Window) -> bool {
+    if input_pass == state.password {
+        let mut is_blocking = state.is_blocking.lock().unwrap();
+        *is_blocking = false;
+        
+        window.set_fullscreen(false).unwrap();
+        window.set_always_on_top(false).unwrap();
+        return true;
     }
+    false
 }
 
 fn main() {
     tauri::Builder::default()
-        .manage(AppState { is_blocking: Arc::new(Mutex::new(false)) })
-        .invoke_handler(tauri::generate_handler![toggle_focus])
+        .manage(AppState { 
+            is_blocking: Arc::new(Mutex::new(false)),
+            password: "123".to_string() 
+        })
+        .invoke_handler(tauri::generate_handler![start_focus, stop_focus])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
